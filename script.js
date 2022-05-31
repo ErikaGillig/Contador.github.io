a9c0a736a0a6b699cb0df4d926df6afb9fe582f1
erika <bambam.gillig@gmail.com>
2022-05-31 18:30:11 -0300

    inicial
    contador inicial

---
+++ script.js
@@ -0,0 +1,24 @@
+const contador = document.getElementById("contar");
+const sumar = document.getElementById("incr");
+const restar = document.getElementById("decr");
+const rest = document.getElementById("reset");
+
+let numero = 0;
+
+sumar.addEventListener("click", ()=>{
+    numero++;
+    contador.innerHTML = numero;
+})
+
+restar.addEventListener("click", ()=>{
+    if (numero==0){}
+    else{
+    numero--;
+    contador.innerHTML = numero;
+    }
+})
+
+reset.addEventListener("click", ()=>{
+    numero=0;
+    contador.innerHTML = numero;
+})
